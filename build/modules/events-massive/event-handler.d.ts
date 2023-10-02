abfecc0f31ca19f7416fd365f8ea94e5d18f33f7
SAC <sac.673@gmail.com>
2023-10-02 15:57:43 +0200

    always build first

--- build/modules/events-massive/event-handler.d.ts
+++ build/modules/events-massive/event-handler.d.ts
@@ -1,15 +1,32 @@
 import { vec2 } from "gl-matrix";
 interface event_data {
     type: string;
+    mode: string | undefined;
     state: string | boolean;
-    type_meta: string;
+    type_meta: string | undefined;
     p1: vec2;
     p2: vec2;
+    p3: vec2;
+    d2: vec2;
     delta: vec2;
+    delta_v: vec2;
     origin: vec2;
+    touch_pos: vec2[];
+    offset: {
+        x: number;
+        y: number;
+        z: number;
+        zd: number;
+    } | undefined;
+    offset_pos: {
+        x: number;
+        y: number;
+        z: number;
+    } | undefined;
     frame: number;
     button: number | undefined;
     interval: number | undefined;
+    mess: string;
 }
 /**
  *
@@ -17,5 +34,5 @@ interface event_data {
  * @param callback
  * @param opt
  */
-declare const event: (target: Element, callback: Function, opt?: unknown) => void;
+declare const event: (target: Element, callback: Function, opt?: unknown) => event_data | undefined;
 export { event, type event_data };
