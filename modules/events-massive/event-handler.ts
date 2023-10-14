7adc2ec4e4eb3d198d787dd238a83751f623ca95
SAC <sac.673@gmail.com>
2023-10-14 12:50:30 +0200

    over and out

--- modules/events-massive/event-handler.ts
+++ modules/events-massive/event-handler.ts
@@ -357,7 +357,8 @@ const event = (target:Element, callback:Function, opt:unknown = opt_default):eve
     if(opts.type === 'screen'){
         target.addEventListener('wheel', (evt) => {event_cursor(callback, evt, opts)}, false);
         target.addEventListener('mouseup', (evt) => {event_cursor(callback, evt, opts)}, false);
-        target.addEventListener('mouseleave', (evt) => {event_cursor(callback, evt, opts)}, false);
+        target.addEventListener('mouseover', (evt) => { event_cursor(callback, evt, opts); }, false);
+        target.addEventListener('mouseout', (evt) => {event_cursor(callback, evt, opts)}, false);
         target.addEventListener('mousedown', (evt) => {event_cursor(callback, evt, opts)}, false);
         target.addEventListener('mousemove', (evt) => {event_cursor(callback, evt, opts)}, false);
         target.addEventListener('touchstart', (evt) => {event_cursor(callback, evt, opts)}, false);
